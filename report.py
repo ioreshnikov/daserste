62f00aefe656efb19ea49d3df915c66d84b7762a
Ivan Oreshnikov <oreshnikov.ivan@gmail.com>
2020-11-22 20:15:31 +0100

    Added emoji, sticker and profanity stats.

--- report.py
+++ report.py
@@ -3,7 +3,9 @@
 
 import argparse
 import json
+import re
 
+import emoji
 import matplotlib.pyplot as plot
 import pandas
 import seaborn
@@ -14,6 +16,21 @@ parser.add_argument(
     "export_data",
     help="path to the exported chat messages (a json file)",
     type=str)
+parser.add_argument(
+    "--timezone-export", "--tze",
+    help="timezone used for timestamps in the export file",
+    type=str,
+    default="Europe/Berlin")
+parser.add_argument(
+    "--timezone-display", "--tzd",
+    help="timezone used in the report",
+    type=str,
+    default="Europe/Moscow")
+parser.add_argument(
+    "--obscene-corpus", "--osc",
+    help="path to the obscenity corpus",
+    type=str,
+    default="obscene_corpus.txt")
 args = parser.parse_args()
 
 
@@ -26,7 +43,53 @@ df = pandas.DataFrame(
     message
     for message in chat["messages"]
     if message["type"] == "message")
+
 df["date"] = pandas.to_datetime(df["date"])
+
+
+# A bit of preprocessing. Let us extract the swearwords. We load the
+# obscenity corpus from the external file.
+with open(args.obscene_corpus) as fd:
+    obscene_corpus = set(line.strip().lower() for line in fd)
+
+# And then we go through every message and do the following:
+# 1. We split the message into individual words.
+# 2. We look for profanity in there.
+# 3. We also extract individual emojis.
+# All this data we dump into individual lists and then we add it to
+# the dataframe as columns.
+
+emojis_column = []
+swearwords_column = []
+words_column = []
+
+for index, row in enumerate(df.itertuples()):
+    if isinstance(row.text, str):
+        text = row.text
+    elif isinstance(row.text, list):
+        for entry in row.text:
+            if isinstance(entry, str):
+                text = entry
+                break
+        else:
+            emojis_column.append([])
+            swearwords_column.append([])
+            words_column.append([])
+            continue
+
+    words = re.findall(r"\w+", text.lower())
+    swearwords = [
+        word for word in words if word in obscene_corpus
+    ]
+    words_column.append(words)
+    swearwords_column.append(swearwords)
+
+    emojis = [char for char in text if char in emoji.UNICODE_EMOJI]
+    emojis_column.append(emojis)
+
+df["emojis"] = pandas.Series(emojis_column)
+df["words"] = pandas.Series(words_column)
+df["swearwords"] = pandas.Series(swearwords_column)
 
 
 # This is an empty context we are going to populate with template
@@ -59,14 +122,33 @@ rate = hour.groupby(hour).size()
 context["daily"] = rate.groupby(lambda ts: ts.hour).mean()
 
 
-plot.figure(1)
-seaborn.lineplot(
-    x=context["timeline"].index,
-    y=context["timeline"].values)
+# 4. Now, the fun part. Top stickers
+context["top_stickers"] = (
+    df[df["media_type"] == "sticker"]
+      .groupby("file")
+      .size()
+      .sort_values(ascending=False)
+      .head(10))
 
-plot.figure(2)
-seaborn.lineplot(
-    x=context["daily"].index,
-    y=context["daily"].values)
 
-plot.show()
+# 5. Top emojis.
+emojis = pandas.Series(df["emojis"].sum())
+context["top_emojis"] = (
+    emojis
+      .groupby(emojis)
+      .size()
+      .sort_values(ascending=False)
+      .head(10))
+
+
+# 6. Top swearwords.
+swearwords = pandas.Series(df["swearwords"].sum())
+context["top_swearwords"] = (
+    swearwords
+      .groupby(swearwords)
+      .size()
+      .sort_values(ascending=False)
+      .head(10))
+
+
+# 7. Swearword density.
